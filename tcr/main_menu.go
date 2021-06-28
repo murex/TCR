d95f08f41e33b095c2d6c3888a8e12c187f21467
Damien Menanteau <damien.menanteau@gmail.com>
2021-06-28 17:24:28 +0200

    Use stty for keyboard capture setting on Unix OS's

--- tcr/main_menu.go
+++ tcr/main_menu.go
@@ -2,8 +2,38 @@
 
 package tcr
 
+import (
+	"bytes"
+	"github.com/mengdaming/tcr/stty"
+	"github.com/mengdaming/tcr/trace"
+	"os"
+)
+
+var initialSttyState bytes.Buffer
+
 func mainMenu() {
 	printOptionsMenu()
 
-	// TODO Add menu handling for non windows OS
+	initialSttyState = stty.SetRaw()
+	defer stty.Restore(&initialSttyState)
+
+	b := make([]byte, 1)
+	for {
+		_, err := os.Stdin.Read(b)
+		if err != nil {
+			trace.Warning(err)
+		}
+		//trace.Info("Read character: ", b)
+
+		switch b[0] {
+		case 'd', 'D':
+			runAsDriver()
+		case 'n', 'N':
+			runAsNavigator()
+		case 'q', 'Q':
+			stty.Restore(&initialSttyState)
+			quit()
+		}
+		printOptionsMenu()
+	}
 }
