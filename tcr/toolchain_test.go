70d68dde0b7d9246012b9b1ed42e676711c7942f
Damien Menanteau <damien.menanteau@gmail.com>
2021-07-10 19:11:14 +0200

    Add test cases for gradle and cmake build execution

--- tcr/toolchain_test.go
+++ tcr/toolchain_test.go
@@ -8,16 +8,28 @@ import (
 )
 
 func TestMain(m *testing.M) {
+	// Prevent trace.Error() from triggering os.Exit()
 	trace.SetTestMode()
 	os.Exit(m.Run())
 }
+
+func runFromDir(t *testing.T, testDir string, testFunction func(t *testing.T)) {
+	initialDir, _ := os.Getwd()
+	_ = os.Chdir(testDir)
+	workDir, _ := os.Getwd()
+	trace.Info("Working directory: ", workDir)
+	testFunction(t)
+	_ = os.Chdir(initialDir)
+}
+
+// Gradle --------------------------------------------------------------------------
 
 func Test_unrecognized_toolchain_name(t *testing.T) {
 	assert.Zero(t, NewToolchain("dummy"))
 	assert.NotZero(t, trace.GetExitReturnCode())
 }
 
-func Test_gradle_toolchain_creation(t *testing.T) {
+func Test_gradle_toolchain_initialization(t *testing.T) {
 	assert.Equal(t, GradleToolchain{}, NewToolchain("gradle"))
 }
 
@@ -30,17 +42,26 @@ func Test_gradle_toolchain_build_command_name(t *testing.T) {
 }
 
 func Test_gradle_toolchain_build_command_args(t *testing.T) {
-	assert.Equal(t, []string{"-x", "test"}, GradleToolchain{}.buildCommandArgs())
+	assert.Equal(t, []string{"build", "-x", "test"}, GradleToolchain{}.buildCommandArgs())
 }
 
-func Test_gradle_toolchain_returns_non_0_when_build_fails(t *testing.T) {
-	// TODO How to setup failing/successful test environments?
-	assert.NotZero(t, GradleToolchain{}.runBuild())
+func Test_gradle_toolchain_returns_error_when_build_fails(t *testing.T) {
+	runFromDir(t,"../test/kata",
+		func(t *testing.T) {
+			assert.NotZero(t, GradleToolchain{}.runBuild())
+		})
 }
 
-// --------------------------------------------------------------------------
+func Test_gradle_toolchain_returns_ok_when_build_passes(t *testing.T) {
+	runFromDir(t,"../test/kata/java",
+		func(t *testing.T) {
+			assert.Zero(t, GradleToolchain{}.runBuild())
+		})
+}
 
-func Test_cmake_toolchain_creation(t *testing.T) {
+// CMake -------------------------------------------------------------------------
+
+func Test_cmake_toolchain_initialization(t *testing.T) {
 	assert.Equal(t, CmakeToolchain{}, NewToolchain("cmake"))
 }
 
@@ -53,4 +74,19 @@ func Test_cmake_toolchain_build_command_args(t *testing.T) {
 		"--build", ".",
 		"--config", "Debug",
 	}, CmakeToolchain{}.buildCommandArgs())
+}
+
+func Test_cmake_toolchain_returns_error_when_build_fails(t *testing.T) {
+	runFromDir(t,"../test/kata",
+		func(t *testing.T) {
+			assert.NotZero(t, CmakeToolchain{}.runBuild())
+		})
+}
+
+// TODO Figure out a way to provide a cmake wrapper
+func test_cmake_toolchain_returns_ok_when_build_passes(t *testing.T) {
+	runFromDir(t,"../test/kata/cpp",
+		func(t *testing.T) {
+			assert.Zero(t, CmakeToolchain{}.runBuild())
+		})
 }
