16b9bc027a10911bc1ce7fb92e0ff86c3009fd7b
Damien Menanteau <damien.menanteau@gmail.com>
2021-07-20 17:16:07 +0200

    Cleanup TODO tags

--- test/kata/java/src/main/java/Dummy.java
+++ test/kata/java/src/main/java/Dummy.java
@@ -1,6 +1,4 @@
 class Dummy {
-
-    // TODO Replace with a method more relevant for the kata
     static int doSomething() {
         return 42;
     }
--- test/kata/java/src/test/java/DummyTest.java
+++ test/kata/java/src/test/java/DummyTest.java
@@ -6,7 +6,6 @@ public class DummyTest {
 
     @Test
     public void acceptance_test() {
-        // TODO Replace with an acceptance test relevant for this kata
         assertEquals(42, Dummy.doSomething());
     }
 }
