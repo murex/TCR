03a0ac30a8f10dc2f392800fa1ff2e1e52c3422f
Damien Menanteau <damien.menanteau@gmail.com>
2021-08-09 01:25:23 +0200

    Add message type report package

--- tcr/report/reporter.go
+++ tcr/report/reporter.go
@@ -1,17 +1,35 @@
 package report
 
 import (
+	"fmt"
 	"github.com/imkira/go-observer"
+	"time"
 )
+
+type MessageType int
+
+const (
+	Simple MessageType = iota
+	Info
+	Header
+	Warning
+	Error
+)
+
+type Message struct {
+	Type      MessageType
+	Text      string
+	Timestamp time.Time
+}
 
 var (
-	msgProperty = observer.NewProperty("")
+	msgProperty = observer.NewProperty(Message{Type: Simple, Text: ""})
 )
 
-func Register(onChange func(msg string)) {
+func Subscribe(onChange func(msg Message)) {
 	stream := msgProperty.Observe()
 
-	val := stream.Value().(string)
+	val := stream.Value().(Message)
 	//fmt.Printf("initial value: %v\n", val)
 
 	for {
@@ -20,15 +38,15 @@ func Register(onChange func(msg string)) {
 		case <-stream.Changes():
 			// advance to next value
 			stream.Next()
-			val = stream.Value().(string)
-			//fmt.Printf("got new value: %v\n", val)
+			val = stream.Value().(Message)
+			fmt.Printf("got new value: %v\n", val)
 			onChange(val)
 		}
 	}
 }
 
-func Report(message string) {
-	//fmt.Println("Reporting message:", message)
+func Report(str string) {
+	var message = Message{Simple, str, time.Now()}
+	fmt.Println("Reporting message:", message)
 	msgProperty.Update(message)
 }
-
--- tcr/report/reporter_test.go
+++ tcr/report/reporter_test.go
@@ -8,31 +8,31 @@ import (
 )
 
 func Test_can_retrieve_reported_message(t *testing.T) {
-	message := "Dummy Message"
-	var result string
+	text := "Dummy Message"
+	var result Message
 	received := make(chan bool)
 
-	go Register(func(msg string) {
+	go Subscribe(func(msg Message) {
 		result = msg
 		received <- true
 	})
 
 	// To make sure the observer is ready to receive
 	time.Sleep(1 * time.Millisecond)
-	Report(message)
+	Report(text)
 	<-received
-	assert.Equal(t, message, result)
+	assert.Equal(t, text, result.Text)
 }
 
-func Test_can_have_multiple_listeners(t *testing.T) {
+func Test_one_message_and_multiple_receivers(t *testing.T) {
 	const nbListeners = 2
-	message := "Dummy Message"
-	var result [nbListeners]string
+	text := "Dummy Message"
+	var result [nbListeners]Message
 	received := make(chan int, nbListeners)
 
 	for i := 0; i < nbListeners; i++ {
 		go func(i int) {
-			go Register(func(msg string) {
+			go Subscribe(func(msg Message) {
 				result[i] = msg
 				received <- i
 			})
@@ -41,28 +41,28 @@ func Test_can_have_multiple_listeners(t *testing.T) {
 
 	// To make sure observers are ready to receive
 	time.Sleep(1 * time.Millisecond)
-	Report(message)
+	Report(text)
 
 	for i := 0; i < nbListeners; i++ {
 		iReceived := <-received
-		assert.Equal(t, message, result[iReceived])
+		assert.Equal(t, text, result[iReceived].Text)
 	}
 }
 
-func Test_can_receive_multiple_messages(t *testing.T) {
+func Test_multiple_messages_and_one_receiver(t *testing.T) {
 	const nbMessages = 2
-	received := make(chan string)
+	received := make(chan Message)
 
-	go Register(func(msg string) {
+	go Subscribe(func(msg Message) {
 		received <- msg
 	})
 
 	// To make sure the observer is ready to receive
 	time.Sleep(1 * time.Millisecond)
 	for i := 0; i < nbMessages; i++ {
-		message := fmt.Sprintf("Dummy Message %v", i)
-		Report(message)
+		text := fmt.Sprintf("Dummy Message %v", i)
+		Report(text)
 		result := <-received
-		assert.Equal(t, message, result)
+		assert.Equal(t, text, result.Text)
 	}
 }
