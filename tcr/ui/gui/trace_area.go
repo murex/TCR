7b85de41a7fe40ebc04021bce2212285c43edef5
Damien Menanteau <damien.menanteau@gmail.com>
2021-08-15 18:40:56 +0200

    Add capping of number of elements in trace

--- tcr/ui/gui/trace_area.go
+++ tcr/ui/gui/trace_area.go
@@ -10,6 +10,10 @@ import (
 	"strings"
 )
 
+const (
+	traceMaxSize = 200
+)
+
 type TraceArea struct {
 	vbox      *fyne.Container
 	container *container.Scroll
@@ -22,39 +26,39 @@ func NewTraceArea() *TraceArea {
 			fyne.TextAlignLeading,
 			fyne.TextStyle{Bold: true},
 		))
-
 	ta.container = container.NewVScroll(ta.vbox)
-
 	return &ta
 }
 
-func (ta *TraceArea) printLine() {
-	ta.vbox.Add(widget.NewSeparator())
-	ta.scrollToBottom()
+func (ta *TraceArea) printSeparator() {
+	ta.print(widget.NewSeparator())
 }
 
 func (ta *TraceArea) printText(col color.Color, a ...interface{}) {
 	for _, s := range strings.Split(fmt.Sprint(a...), "\n") {
-		ta.vbox.Add(canvas.NewText(s, col))
+		ta.print(canvas.NewText(s, col))
 	}
-	ta.scrollToBottom()
 }
 
 func (ta *TraceArea) printHeader(a ...interface{}) {
-	ta.printLine()
-	ta.vbox.Add(widget.NewLabelWithStyle(
+	ta.printSeparator()
+	ta.print(widget.NewLabelWithStyle(
 		fmt.Sprint(a...),
 		fyne.TextAlignLeading,
 		fyne.TextStyle{Bold: true},
 	))
-	ta.printLine()
+	ta.printSeparator()
+}
+
+func (ta *TraceArea) print(object fyne.CanvasObject) {
+	// We cap the number of lines in trace to prevent performance decrease as trace piles up
+	if len(ta.vbox.Objects) >= traceMaxSize {
+		ta.vbox.Objects = ta.vbox.Objects[1:]
+	}
+	ta.vbox.Add(object)
 	ta.scrollToBottom()
 }
 
 func (ta *TraceArea) scrollToBottom() {
-	// The ScrollToTop() call below is some kind of workaround to ensure
-	// that the UI indeed refreshes and scrolls to bottom when ScrollToBottom() is called
-	ta.container.ScrollToTop()
 	ta.container.ScrollToBottom()
 }
-
