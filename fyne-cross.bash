4eb7fc9ffa36f862d6794217c72661ffed9963dc
Damien Menanteau <damien.menanteau@gmail.com>
2021-08-30 16:51:34 +0200

    Automatically retrieve TCR version from application itself

--- fyne-cross.bash
+++ fyne-cross.bash
@@ -6,14 +6,22 @@ base_dir="$(cd "$(dirname -- "$0")" && pwd)"
 
 cd "${base_dir}"
 
+if ! type go >/dev/null 2>/dev/null; then
+  echo "go is required to run this script"
+  echo "Refer to https://golang.org/ for installation and setup"
+  exit 1
+fi
+
 if ! type fyne-cross >/dev/null 2>/dev/null; then
   echo "fyne-cross command is required to run this script"
   echo "Refer to https://github.com/fyne-io/fyne-cross for installation and setup"
   exit 1
 fi
 
-fyne-cross darwin --app-id tcr --app-version 0.3.1 -arch=*
-fyne-cross linux --app-id tcr --app-version 0.3.1 -arch=*
-fyne-cross windows --app-id tcr --app-version 0.3.1 -arch=*
+app_id="tcr"
+app_version=$(go run . --version | cut -f3 -d' ')
+echo "Building TCR version ${app_version} for all targets"
 
-
+fyne-cross darwin --app-id ${app_id} --app-version "${app_version}" -arch=*
+fyne-cross linux --app-id ${app_id} --app-version "${app_version}" -arch=*
+fyne-cross windows --app-id ${app_id} --app-version "${app_version}" -arch=*
