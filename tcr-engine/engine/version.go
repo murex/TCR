8533b5c5af264cc03e1f38592731648a3ae32ef4
Damien Menanteau <damien.menanteau@gmail.com>
2021-09-18 18:00:42 +0200

    Set version to 0.4.0

--- tcr-engine/engine/version.go
+++ tcr-engine/engine/version.go
@@ -2,4 +2,4 @@ package engine
 
 // Version is TCR application version.
 // Its value must always be in sync with the git tag used when releasing a new version
-const Version = "0.3.3"
+const Version = "0.4.0"
