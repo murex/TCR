f20ef05f18e38b9d2e89a91abed7d844549eed32
Damien Menanteau <damien.menanteau@gmail.com>
2021-10-01 18:20:57 +0200

    Add a features file for turning on/off new features

---
+++ tcr-engine/engine/features.go
@@ -0,0 +1,11 @@
+package engine
+
+// Feature toggles allowing to quickly turn on/off a feature
+
+const (
+	// EnableTcrInactivityTeaser Turns on/off TCR inactivity teaser messages
+	EnableTcrInactivityTeaser = false
+
+	// EnableMobTimer Turns on/off mob countdown timer
+	EnableMobTimer = true
+)
