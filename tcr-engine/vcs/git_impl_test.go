516196d92f7f6a762d285a239a4654e059405386
Damien Menanteau <damien.menanteau@gmail.com>
2022-05-16 12:36:16 +0200

    Add test cases for GitImpl commit and restore functions

--- tcr-engine/vcs/git_impl_test.go
+++ tcr-engine/vcs/git_impl_test.go
@@ -61,20 +61,34 @@ func Test_git_diff_command(t *testing.T) {
 	testFlags := []struct {
 		desc          string
 		gitDiffOutput string
-		expected      []FileDiff
+		gitDiffError  error
+		expectError   bool
+		expectedDiff  []FileDiff
 	}{
+		{"git error",
+			"",
+			errors.New("git diff error"),
+			true,
+			nil,
+		},
 		{"0 file changed",
 			"",
+			nil,
+			false,
 			nil,
 		},
 		{"1 file changed",
 			"1\t1\tsome-file.txt\n",
+			nil,
+			false,
 			[]FileDiff{
 				{"some-file.txt", 1, 1},
 			},
 		},
 		{"2 files changed",
 			"1\t1\tfile1.txt\n1\t1\tfile2.txt\n",
+			nil,
+			false,
 			[]FileDiff{
 				{"file1.txt", 1, 1},
 				{"file2.txt", 1, 1},
@@ -82,18 +96,24 @@ func Test_git_diff_command(t *testing.T) {
 		},
 		{"file changed in sub-directory",
 			"1\t1\tsome-dir/some-file.txt\n",
+			nil,
+			false,
 			[]FileDiff{
 				{filepath.Join("some-dir", "some-file.txt"), 1, 1},
 			},
 		},
 		{"1 file changed with added lines only",
 			"15\t0\tsome-file.txt\n",
+			nil,
+			false,
 			[]FileDiff{
 				{"some-file.txt", 15, 0},
 			},
 		},
 		{"1 file changed with removed lines only",
 			"0\t7\tsome-file.txt\n",
+			nil,
+			false,
 			[]FileDiff{
 				{"some-file.txt", 0, 7},
 			},
@@ -104,11 +124,16 @@ func Test_git_diff_command(t *testing.T) {
 			git := &GitImpl{
 				// git command calls are faked
 				runGitFunction: func(_ []string) (output []byte, err error) {
-					return []byte(tt.gitDiffOutput), nil
+					return []byte(tt.gitDiffOutput), tt.gitDiffError
 				},
 			}
-			fileDiffs, _ := git.Diff()
-			assert.Equal(t, tt.expected, fileDiffs)
+			fileDiffs, err := git.Diff()
+			if tt.expectError {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+			assert.Equal(t, tt.expectedDiff, fileDiffs)
 		})
 	}
 }
@@ -212,6 +237,76 @@ func Test_git_pull_command(t *testing.T) {
 				workingBranchExistsOnRemote: tt.branchOnRemote,
 			}
 			err := git.Pull()
+			if tt.expectError {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+		})
+	}
+}
+
+func Test_git_commit_command(t *testing.T) {
+	testFlags := []struct {
+		desc        string
+		gitError    error
+		expectError bool
+	}{
+		{
+			"no git error",
+			nil,
+			false,
+		},
+		{
+			"git error",
+			errors.New("git commit error"),
+			false, // We currently ignore git commit errors to handle the case when there's nothing to commit
+		},
+	}
+	for _, tt := range testFlags {
+		t.Run(tt.desc, func(t *testing.T) {
+			git := &GitImpl{
+				// git command calls are faked
+				traceGitFunction: func(_ []string) (err error) {
+					return tt.gitError
+				},
+			}
+			err := git.Commit()
+			if tt.expectError {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+		})
+	}
+}
+
+func Test_git_restore_command(t *testing.T) {
+	testFlags := []struct {
+		desc        string
+		gitError    error
+		expectError bool
+	}{
+		{
+			"no git error",
+			nil,
+			false,
+		},
+		{
+			"git error",
+			errors.New("git restore error"),
+			true,
+		},
+	}
+	for _, tt := range testFlags {
+		t.Run(tt.desc, func(t *testing.T) {
+			git := &GitImpl{
+				// git command calls are faked
+				traceGitFunction: func(_ []string) (err error) {
+					return tt.gitError
+				},
+			}
+			err := git.Restore("some-path")
 			if tt.expectError {
 				assert.Error(t, err)
 			} else {
