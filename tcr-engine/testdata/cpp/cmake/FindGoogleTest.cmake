2c42e5a9be66b2e5ba84c736e0bba7d13aecb6f3
Damien Menanteau <damien.menanteau@gmail.com>
2022-06-14 14:40:56 +0200

    Align testdata contents with xunit-compatible toolchains

--- tcr-engine/testdata/cpp/cmake/FindGoogleTest.cmake
+++ tcr-engine/testdata/cpp/cmake/FindGoogleTest.cmake
@@ -23,6 +23,6 @@
 CPMFindPackage(
   NAME googletest
   GITHUB_REPOSITORY google/googletest
-  GIT_TAG master
+  GIT_TAG main
   OPTIONS "INSTALL_GTEST OFF" "gtest_force_shared_crt" "BUILD_GMOCK OFF"
 )
