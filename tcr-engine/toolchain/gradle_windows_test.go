dc3712297d4f9990bcab5d0b12a43fb7ad0b9d7b
Damien Menanteau <damien.menanteau@gmail.com>
2022-11-14 12:11:52 +0100

    [#174] Merge gradle and gradle-wrapper toolchain tests
    To prevent replications between test cases on the 2 toolchains

--- tcr-engine/toolchain/gradle_windows_test.go
+++ tcr-engine/toolchain/gradle_windows_test.go
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2021 Murex
+Copyright (c) 2022 Murex
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -22,14 +22,4 @@ SOFTWARE.
 
 package toolchain
 
-import (
-	"testing"
-)
-
-func Test_gradle_wrapper_toolchain_build_command_path_on_windows(t *testing.T) {
-	assertBuildCommandPath(t, ".\\gradlew.bat", gradleWrapperToolchainName)
-}
-
-func Test_gradle_wrapper_toolchain_test_command_path_on_windows(t *testing.T) {
-	assertTestCommandPath(t, ".\\gradlew.bat", gradleWrapperToolchainName)
-}
+const gradleWrapperCommandPath = ".\\gradlew.bat"
