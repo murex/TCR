fa48f8fc4c44e8e6dc57ce5cd209435c7d81c53e
Damien Menanteau <damien.menanteau@gmail.com>
2022-11-28 17:37:02 +0100

    [#119] Add example with C# language and dotnet toolchain

---
+++ examples/csharp-dotnet/src/HelloWorld/HelloWorldClass.cs
@@ -0,0 +1,10 @@
+namespace HelloWorld
+{
+    public class HelloWorldClass
+    {
+        public static string SayHello(string name)
+        {
+            return "Hello " + name + "!";
+        }
+    }
+}
