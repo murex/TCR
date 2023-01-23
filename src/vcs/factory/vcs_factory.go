d279937bc7ecb3726a6206df2c9a4718ba87d634
Damien Menanteau <damien.menanteau@gmail.com>
2023-01-23 18:41:04 +0100

    [#200] add vcs.factory package

---
+++ src/vcs/factory/vcs_factory.go
@@ -0,0 +1,53 @@
+/*
+Copyright (c) 2023 Murex
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+package factory
+
+import (
+	"fmt"
+	"github.com/murex/tcr/vcs"
+	"github.com/murex/tcr/vcs/git"
+	"github.com/murex/tcr/vcs/p4"
+	"strings"
+)
+
+// UnsupportedVCSError is returned when the provided VCS name is not supported by the factory.
+type UnsupportedVCSError struct {
+	vcsName string
+}
+
+// Error returns the error description
+func (e *UnsupportedVCSError) Error() string {
+	return fmt.Sprintf("VCS not supported: \"%s\"", e.vcsName)
+}
+
+// InitVCS returns the VCS instance of type defined by name, working on the provided directory
+func InitVCS(name string, dir string) (vcs.Interface, error) {
+	switch strings.ToLower(name) {
+	case git.Name:
+		return git.New(dir)
+	case p4.Name:
+		return p4.New(dir)
+	default:
+		return nil, &UnsupportedVCSError{name}
+	}
+}
