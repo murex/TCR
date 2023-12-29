c6aade06e9bfa6c873270c36ed261e51a7637239
Damien Menanteau <damien.menanteau@gmail.com>
2023-12-29 09:53:40 +0100

    Add embedded terminal in console compnent

--- webapp/src/app/components/tcr-console/tcr-console.component.ts
+++ webapp/src/app/components/tcr-console/tcr-console.component.ts
@@ -1,33 +1,37 @@
-import {Component} from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
 import {WebsocketService} from "../../services/websocket.service";
 import {catchError, retry, throwError} from "rxjs";
 import {takeUntilDestroyed} from "@angular/core/rxjs-interop";
-import {NgIf} from "@angular/common";
+import {NgTerminal, NgTerminalModule} from "ng-terminal";
 
 @Component({
   selector: 'app-tcr-console',
   standalone: true,
   imports: [
-    NgIf
+    NgTerminalModule
   ],
   templateUrl: './tcr-console.component.html',
   styleUrl: './tcr-console.component.css'
 })
 export class TcrConsoleComponent {
   title = "TCR Console";
-  public trace: String = "";
+  @ViewChild('term', {static: false}) child!: NgTerminal;
 
   constructor(private ws: WebsocketService) {
     this.ws.webSocket$
       .pipe(
         catchError((error) => {
-          this.trace = "";
           return throwError(() => new Error(error));
         }),
         retry({delay: 5_000}),
         takeUntilDestroyed())
       .subscribe((value) => {
-        this.trace += value + "\n";
+        this.write(value);
       });
   }
+
+  private write(input: string) {
+    this.child.write(input + "\r\n");
+  };
+
 }
