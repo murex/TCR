98dbc0c86288ffff26b15b71d5e8f27c9deb66d3
Damien Menanteau <damien.menanteau@gmail.com>
2024-02-09 16:24:54 +0100

    Add tests on ui.multicaster

--- src/ui/multicaster_test.go
+++ src/ui/multicaster_test.go
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2023 Murex
+Copyright (c) 2024 Murex
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -22,4 +22,117 @@ SOFTWARE.
 
 package ui
 
-// TODO add unit tests on multiplexer
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_primary_user_interface_registration(t *testing.T) {
+	multicaster := NewMulticaster()
+	ui := NewFakeUI()
+	multicaster.Register(ui, true)
+
+	assert.Equal(t, []UserInterface{ui}, multicaster.uiList)
+	assert.Equal(t, ui, multicaster.uiPrimary)
+}
+
+func Test_secondary_user_interface_registration(t *testing.T) {
+	multicaster := NewMulticaster()
+	ui := NewFakeUI()
+	multicaster.Register(ui, false)
+
+	assert.Equal(t, []UserInterface{ui}, multicaster.uiList)
+	assert.NotEqual(t, ui, multicaster.uiPrimary)
+}
+
+func Test_multiple_user_interfaces_registration(t *testing.T) {
+	multicaster := NewMulticaster()
+	ui1 := NewFakeUI()
+	ui2 := NewFakeUI()
+	multicaster.Register(ui1, false)
+	multicaster.Register(ui2, false)
+
+	assert.Equal(t, []UserInterface{ui1, ui2}, multicaster.uiList)
+}
+
+func Test_multicasting_ui_messages(t *testing.T) {
+	var multicaster *Multicaster
+	testFlags := []struct {
+		desc      string
+		operation func()
+		expected  Call
+	}{
+		{
+			"start",
+			func() { multicaster.Start() },
+			CallStart,
+		},
+		{
+			"show running mode",
+			func() { multicaster.ShowRunningMode(nil) },
+			CallShowRunningMode,
+		},
+		{
+			"notify role starting",
+			func() { multicaster.NotifyRoleStarting(nil) },
+			CallNotifyRoleStarting,
+		},
+		{
+			"notify role ending",
+			func() { multicaster.NotifyRoleEnding(nil) },
+			CallNotifyRoleEnding,
+		},
+		{
+			"show session info",
+			func() { multicaster.ShowSessionInfo() },
+			CallShowSessionInfo,
+		},
+		{
+			"start reporting",
+			func() { multicaster.StartReporting() },
+			CallStartReporting,
+		},
+		{
+			"stop reporting",
+			func() { multicaster.StopReporting() },
+			CallStopReporting,
+		},
+		{
+			"mute desktop notifications",
+			func() { multicaster.MuteDesktopNotifications(true) },
+			CallMuteDesktopNotifications,
+		},
+	}
+
+	for _, tt := range testFlags {
+		t.Run(tt.desc, func(t *testing.T) {
+			multicaster = NewMulticaster()
+			uis := []*FakeUI{NewFakeUI(), NewFakeUI()}
+			for i, ui := range uis {
+				multicaster.Register(ui, i == 0)
+			}
+
+			tt.operation()
+
+			for _, ui := range uis {
+				assert.Contains(t, ui.GetCallHistory(), tt.expected)
+			}
+		})
+	}
+}
+
+func Test_sending_confirm_message_to_primary_ui_only(t *testing.T) {
+	multicaster := NewMulticaster()
+	primary := NewFakeUI()
+	secondary1 := NewFakeUI()
+	secondary2 := NewFakeUI()
+	multicaster.Register(primary, true)
+	multicaster.Register(secondary1, false)
+	multicaster.Register(secondary2, false)
+
+	multicaster.Confirm("some question", true)
+
+	assert.Contains(t, primary.GetCallHistory(), CallConfirm)
+	assert.NotContains(t, secondary1.GetCallHistory(), CallConfirm)
+	assert.NotContains(t, secondary2.GetCallHistory(), CallConfirm)
+}
--- src/ui/user_interface_test_fake.go
+++ src/ui/user_interface_test_fake.go
@@ -1,7 +1,7 @@
 //go:build test_helper
 
 /*
-Copyright (c) 2022 Murex
+Copyright (c) 2024 Murex
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -29,41 +29,86 @@ import (
 	"github.com/murex/tcr/runmode"
 )
 
+// Call is used to track calls to UI operations
+type Call string
+
+// Possible values for Call
+const (
+	CallStart                    Call = "start"
+	CallShowRunningMode          Call = "show-running-mode"
+	CallNotifyRoleStarting       Call = "notify-role-starting"
+	CallNotifyRoleEnding         Call = "notify-role-ending"
+	CallShowSessionInfo          Call = "show-session-info"
+	CallConfirm                  Call = "confirm"
+	CallStartReporting           Call = "start-reporting"
+	CallStopReporting            Call = "stop-reporting"
+	CallMuteDesktopNotifications Call = "mute-desktop-notifications"
+)
+
+var NoCall []Call
+
 // FakeUI provides a fake UI for running tests. It basically does nothing
 // apart from stubbing out all UI behaviors
 type FakeUI struct {
+	callRecord []Call
 }
 
 // NewFakeUI creates a new instance of a fake UI
-func NewFakeUI() UserInterface {
+func NewFakeUI() *FakeUI {
 	return &FakeUI{}
 }
 
+func (ui *FakeUI) recordCall(call Call) {
+	ui.callRecord = append(ui.callRecord, call)
+}
+
+// GetCallHistory returns the list of Call events tracked by FakeUI
+func (ui *FakeUI) GetCallHistory() []Call {
+	return ui.callRecord
+}
+
 // Start does nothing in FakeUI
-func (ui FakeUI) Start() {}
+func (ui *FakeUI) Start() {
+	ui.recordCall(CallStart)
+}
 
 // ShowRunningMode does nothing in FakeUI
-func (ui FakeUI) ShowRunningMode(_ runmode.RunMode) {}
+func (ui *FakeUI) ShowRunningMode(_ runmode.RunMode) {
+	ui.recordCall(CallShowRunningMode)
+}
 
 // NotifyRoleStarting does nothing in FakeUI
-func (ui FakeUI) NotifyRoleStarting(_ role.Role) {}
+func (ui *FakeUI) NotifyRoleStarting(_ role.Role) {
+	ui.recordCall(CallNotifyRoleStarting)
+}
 
 // NotifyRoleEnding does nothing in FakeUI
-func (ui FakeUI) NotifyRoleEnding(_ role.Role) {}
+func (ui *FakeUI) NotifyRoleEnding(_ role.Role) {
+	ui.recordCall(CallNotifyRoleEnding)
+}
 
 // ShowSessionInfo does nothing in FakeUI
-func (ui FakeUI) ShowSessionInfo() {}
+func (ui *FakeUI) ShowSessionInfo() {
+	ui.recordCall(CallShowSessionInfo)
+}
 
 // Confirm always returns true in FakeUI
-func (ui FakeUI) Confirm(_ string, _ bool) bool {
+func (ui *FakeUI) Confirm(_ string, _ bool) bool {
+	ui.recordCall(CallConfirm)
 	return true
 }
 
 // StartReporting does nothing in FakeUI
-func (ui FakeUI) StartReporting() {}
+func (ui *FakeUI) StartReporting() {
+	ui.recordCall(CallStartReporting)
+}
 
 // StopReporting does nothing in FakeUI
-func (ui FakeUI) StopReporting() {}
+func (ui *FakeUI) StopReporting() {
+	ui.recordCall(CallStopReporting)
+}
 
 // MuteDesktopNotifications does nothing in FakeUI
-func (ui FakeUI) MuteDesktopNotifications(_ bool) {}
+func (ui *FakeUI) MuteDesktopNotifications(_ bool) {
+	ui.recordCall(CallMuteDesktopNotifications)
+}
