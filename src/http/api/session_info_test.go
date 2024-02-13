90879a9359dece38a1ef5f2e2238c4152f1d31ce
Damien Menanteau <damien.menanteau@gmail.com>
2024-02-13 15:46:11 +0100

    Add tests for http.api.session_info

---
+++ src/http/api/session_info_test.go
@@ -0,0 +1,66 @@
+/*
+Copyright (c) 2024 Murex
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+package api
+
+import (
+	"encoding/json"
+	"github.com/gin-gonic/gin"
+	"github.com/murex/tcr/engine"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func Test_session_info_get_handler(t *testing.T) {
+	// Setup TCR instance using Fake TCR Engine
+	SetTCRInstance(engine.NewFakeTCREngine())
+	// Setup the router
+	rPath := "/api/session-info"
+	router := gin.Default()
+	router.GET(rPath, SessionInfoGetHandler)
+
+	// Prepare the request, send it and capture the response
+	req, _ := http.NewRequest("GET", rPath, nil)
+	w := httptest.NewRecorder()
+	router.ServeHTTP(w, req)
+
+	// Verify the response's code, header and body
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.Equal(t, "application/json; charset=utf-8", w.Header().Get("Content-Type"))
+	info := tcr.GetSessionInfo()
+	expected := sessionInfo{
+		BaseDir:           info.BaseDir,
+		WorkDir:           info.WorkDir,
+		LanguageName:      info.LanguageName,
+		ToolchainName:     info.ToolchainName,
+		VCSName:           info.VCSName,
+		VCSSessionSummary: info.VCSSessionSummary,
+		CommitOnFail:      info.CommitOnFail,
+		GitAutoPush:       info.GitAutoPush,
+		MessageSuffix:     info.MessageSuffix,
+	}
+	var actual sessionInfo
+	assert.NoError(t, json.Unmarshal(w.Body.Bytes(), &actual))
+	assert.Equal(t, expected, actual)
+}
