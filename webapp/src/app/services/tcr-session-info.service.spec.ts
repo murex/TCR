5ad918c5d6d48a77ea83f4c9b9ad47462ef3e5da
Damien Menanteau <damien.menanteau@gmail.com>
2024-03-13 18:44:33 +0100

    Add unit tests on webapp's TCR Session Info service

--- webapp/src/app/services/tcr-session-info.service.spec.ts
+++ webapp/src/app/services/tcr-session-info.service.spec.ts
@@ -1,16 +1,56 @@
 import {TestBed} from '@angular/core/testing';
-
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
 import {TcrSessionInfoService} from './tcr-session-info.service';
+import {TcrSessionInfo} from '../interfaces/tcr-session-info';
 
-xdescribe('TcrSessionInfoService', () => {
+describe('TcrSessionInfoService', () => {
   let service: TcrSessionInfoService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [TcrSessionInfoService]
+    });
+
     service = TestBed.inject(TcrSessionInfoService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  it('returns expected session info when getSessionInfo is called', () => {
+    const sample: TcrSessionInfo = {
+      baseDir: "/my/base/dir",
+      commitOnFail: false,
+      gitAutoPush: false,
+      language: "java",
+      messageSuffix: "my-suffix",
+      toolchain: "gradle",
+      vcsName: "git",
+      vcsSession: "my VCS session",
+      workDir: "/my/work/dir"
+    };
+
+    service.getSessionInfo().subscribe(sessionInfo => {
+      expect(sessionInfo).toEqual(sample);
+    });
+
+    const req = httpMock.expectOne(`/api/session-info`);
+    expect(req.request.method).toBe('GET');
+    expect(req.request.responseType).toEqual('json');
+    req.flush(sample);
+  });
+
+  it('returns undefined when getSessionInfo receives an error response', () => {
+    service.getSessionInfo().subscribe(sessionInfo => {
+      expect(sessionInfo).toBeUndefined();
+    });
+
+    const req = httpMock.expectOne(`/api/session-info`);
+    expect(req.request.method).toBe('GET');
+    req.flush(null, {status: 500, statusText: 'Server error'})
   });
 });
