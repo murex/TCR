965f82ff7ed95ebd23f590bb35ab0ec39071bd76
Damien Menanteau <damien.menanteau@gmail.com>
2024-03-17 23:35:12 +0100

    Cleanup webapp's main component imports

--- webapp/src/app/app.component.ts
+++ webapp/src/app/app.component.ts
@@ -2,8 +2,6 @@ import {Component} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RouterLink, RouterModule, RouterOutlet} from '@angular/router';
 import {HttpClientModule} from "@angular/common/http";
-import {TcrAboutComponent} from "./components/tcr-about/tcr-about.component";
-import {TcrSessionInfoComponent} from "./components/tcr-session-info/tcr-session-info.component";
 import {HeaderComponent} from "./components/header/header.component";
 import {FooterComponent} from "./components/footer/footer.component";
 
@@ -13,8 +11,6 @@ import {FooterComponent} from "./components/footer/footer.component";
   imports: [
     CommonModule,
     RouterOutlet,
-    TcrAboutComponent,
-    TcrSessionInfoComponent,
     RouterLink,
     RouterModule,
     HttpClientModule,
