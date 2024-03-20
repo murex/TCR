77462862e07fe07ae544d6b95ec482e32ece9d5d
Damien Menanteau <damien.menanteau@gmail.com>
2024-03-20 18:29:25 +0100

    Add unit tests on webapp's app-component

--- webapp/src/app/app.component.spec.ts
+++ webapp/src/app/app.component.spec.ts
@@ -1,23 +1,47 @@
-import {TestBed} from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 import {AppComponent} from './app.component';
+import {RouterModule} from "@angular/router";
+import {HttpClientTestingModule} from "@angular/common/http/testing";
 
-xdescribe('AppComponent', () => {
+describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [AppComponent, HttpClientTestingModule, RouterModule.forRoot([])],
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+  describe('component instance', () => {
+    it('should create the app', () => {
+      const fixture = TestBed.createComponent(AppComponent);
+      const app = fixture.componentInstance;
+      expect(app).toBeTruthy();
+    });
+
+    it(`should have TCR for title`, () => {
+      const fixture = TestBed.createComponent(AppComponent);
+      const app = fixture.componentInstance;
+      expect(app.title).toEqual('TCR');
+    });
   });
 
-  it(`should have the 'tcr' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('TCR');
-  });
+  describe('component DOM', () => {
+    const testCases = [
+      {selector: 'app-header', description: 'a header element'},
+      {selector: 'router-outlet', description: 'a router outlet element'},
+      {selector: 'app-footer', description: 'a footer element'},
+    ];
 
+    testCases.forEach(({selector, description}) => {
+      it(`should have ${description}`, () => {
+        expect(fixture.nativeElement.querySelector(selector)).toBeTruthy();
+      });
+    });
+  });
 });
