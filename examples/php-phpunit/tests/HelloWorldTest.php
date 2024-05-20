14691b2317842f798739ddfd4f2e5856b1db9db0
Damien Menanteau <damien.menanteau@gmail.com>
2024-05-20 10:10:15 +0200

    Bump php from 8.1 to 8.3 in /examples/php-*

--- examples/php-phpunit/tests/HelloWorldTest.php
+++ examples/php-phpunit/tests/HelloWorldTest.php
@@ -7,13 +7,11 @@ namespace HelloWorld\Tests;
 use HelloWorld\HelloWorld;
 use PHPUnit\Framework\TestCase;
 
-class HelloWorldTest extends TestCase
+final class HelloWorldTest extends TestCase
 {
-    /** @test */
     public function test_say_hello(): void
     {
-        $helloWorld = new HelloWorld();
+        $helloWorld = new HelloWorld;
         $this->assertEquals("Hello Sue!", $helloWorld->sayHello("Sue"));
     }
-
 }
