b3f07c25f17a95671423182a9fc9c735f483f40e
Damien Menanteau <damien.menanteau@gmail.com>
2024-07-02 16:58:16 +0200

    ✅ TCR - tests passing
    changed-lines:
        src: 16
        test: 0
    test-stats:
        run: 1
        passed: 1
        failed: 0
        skipped: 0
        error: 0
        duration: 16ms

--- src/testdata/java/src/main/java/Dummy.java
+++ src/testdata/java/src/main/java/Dummy.java
@@ -1,13 +1,13 @@
 class Dummy {
     static int doSomething() {
-        for (int i = 0; i < 10; i++) {
-            System.out.println("doing something...");
-            try {
-                Thread.sleep(100);
-            } catch (InterruptedException e) {
-                System.out.println("got interrupted!");
-            }
-        }
+//         for (int i = 0; i < 10; i++) {
+//             System.out.println("doing something...");
+//             try {
+//                 Thread.sleep(100);
+//             } catch (InterruptedException e) {
+//                 System.out.println("got interrupted!");
+//             }
+//         }
         return 42;
     }
 }
