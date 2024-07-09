18e52aae67fcb61b6254b14bade04e9e57bc9de4
Damien Menanteau <damien.menanteau@gmail.com>
2024-07-09 16:52:21 +0200

    [#694] Add new webapp service for sending control requests

---
+++ webapp/src/app/services/tcr-controls.service.ts
@@ -0,0 +1,45 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {catchError, Observable, of} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TcrControlsService {
+  private apiUrl: string = '/api'; // URL to web api
+
+  constructor(private http: HttpClient) {
+  }
+
+  abortCommand(): Observable<Object> {
+    return this.sendControl(`abort-command`);
+  }
+
+  private sendControl(command: string) {
+    const url: string = `${this.apiUrl}/controls/${command}`;
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+      })
+    };
+
+    return this.http.post(url, httpOptions).pipe(
+      catchError(this.handleError<Object>(command)),
+    );
+  }
+
+  /**
+   * Handle HTTP operation that failed.
+   * Let the app continue.
+   *
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T>(operation: string, result?: T) {
+    return (error: unknown): Observable<T> => {
+      console.error(`${operation} - ` + error);
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+}
