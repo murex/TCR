c279569d80c6423d0167dd3bc3f9b09ba409b10f
Damien Menanteau <damien.menanteau@gmail.com>
2025-10-06 23:32:06 +0200

    Bump bazel from 6.2.0 to 8.4.2 in /examples/python-bazel

--- examples/python-bazel/tests/test_hello_world.py
+++ examples/python-bazel/tests/test_hello_world.py
@@ -4,4 +4,4 @@ from hello_world.hello_world import say_hello
 class TestHelloWorld:
 
     def test_say_hello(self) -> None:
-        assert ('Hello Sue!' == say_hello('Sue'))
+        assert 'Hello Sue!' == say_hello('Sue')
