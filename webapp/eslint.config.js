ce842b830aaeb774a7710641aec9ad071142ade3
Damien Menanteau <damien.menanteau@gmail.com>
2026-02-12 07:33:17 +0100

    Upgrade Angular to 21.1.4 and switch ESLint
    - Remove legacy ESLint config and add new eslint.config.js
    - Upgrade Angular to 21.1.4 across dependencies and refresh package-lock
    - Add ESLint tooling: eslint, jsdom, typescript-eslint, and
      angular-eslint
    - Configure ESLint to use TypeScript ESLint and angular-eslint with
      inline
      template support

---
+++ webapp/eslint.config.js
@@ -0,0 +1,55 @@
+// @ts-check
+const eslint = require("@eslint/js");
+const tseslint = require("typescript-eslint");
+const angular = require("angular-eslint");
+
+module.exports = tseslint.config(
+  {
+    files: ["**/*.ts"],
+    extends: [
+      eslint.configs.recommended,
+      ...tseslint.configs.recommended,
+      ...angular.configs.tsRecommended,
+    ],
+    processor: angular.processInlineTemplates,
+    rules: {
+      "@angular-eslint/directive-selector": [
+        "error",
+        {
+          type: "attribute",
+          prefix: "app",
+          style: "camelCase",
+        },
+      ],
+      "@angular-eslint/component-selector": [
+        "error",
+        {
+          type: "element",
+          prefix: "app",
+          style: "kebab-case",
+        },
+      ],
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          caughtErrorsIgnorePattern: "^_",
+        },
+      ],
+      "@angular-eslint/prefer-inject": "off",
+    },
+  },
+  {
+    files: ["**/*.html"],
+    extends: [
+      ...angular.configs.templateRecommended,
+      ...angular.configs.templateAccessibility,
+    ],
+    rules: {
+      "@angular-eslint/template/interactive-supports-focus": "off",
+      "@angular-eslint/template/click-events-have-key-events": "off",
+    },
+  },
+);
